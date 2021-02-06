a7a0e6ea730de9484225859cea7a687bdb326f4b
adombence <adom.bence97@gmail.com>
2021-02-06 17:34:03 +0100

    variables

---
+++ Variables-data-types/src/main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val wholeNumber = 3
+    val bigNumber = 3L
+    val preciseDecimal = 3.33
+    val decimal = 3.33f
+    val boolean = false
+    val char = 'A'
+
+    println("wholeNUmber: $wholeNumber")
+    println("bigNumber: $bigNumber")
+    println("preciseDecimal: $preciseDecimal")
+    println("deciaml: $decimal")
+    println("boolean: $boolean")
+    println("char: $char")
+
+
+    val x = 3f / 4f
+    println("The value of x is $x")
+
+    val y = 10 % 2
+    println("The value of y is $y")
+
+    val z = 10 * 5
+    println("The value of z is $z")
+}
